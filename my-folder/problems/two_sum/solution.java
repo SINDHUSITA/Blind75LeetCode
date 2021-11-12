27d23f2cd4b4be6cd07a3359f5c2eeb5ccb12a1f
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2021-11-12 23:53:50 +0000

    Sync LeetCode submission - Two Sum (java)

--- my-folder/problems/two_sum/solution.java
+++ my-folder/problems/two_sum/solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       // List k = new ArrayList();
-        for (int i=0;i<nums.length;i++){
-            for (int j=i+1;j<nums.length;j++){
-                if (nums[i]+nums[j] == target){
-                    
-                    return new int[]{i,j};
+        int[] output = new int[2];
+        for(int i = 0; i<nums.length-1; i++){
+            for(int j = i+1; j<nums.length;j++){
+                if((nums[i]+nums[j]) == target){
+                    output[0] = i;
+                    output[1] = j;
+                    return output;
                 }
-            } 
-        } 
-        throw new IllegalArgumentException("No two sum solution");
+            }
+        }
+        return null;
     }
 }
