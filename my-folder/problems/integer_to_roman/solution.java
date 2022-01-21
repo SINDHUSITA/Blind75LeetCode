4becaa6311171d223a7c9e938ea8ccd90730d303
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2022-01-21 16:17:49 +0000

    Sync LeetCode submission - Integer to Roman (java)

--- my-folder/problems/integer_to_roman/solution.java
+++ my-folder/problems/integer_to_roman/solution.java
@@ -1,10 +1,16 @@
-class Solution { 
+class Solution {
     private static final String[] thousands = {"", "M", "MM", "MMM"};
     private static final String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}; 
     private static final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
     private static final String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
     
     public String intToRoman(int num) { 
-        return thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10];
+        StringBuilder sb = new StringBuilder();
+        sb.append(thousands[num / 1000]);
+        sb.append(hundreds[num % 1000 / 100]);
+        sb.append(tens[num % 100 / 10]);
+        sb.append(ones[num % 10]);
+        
+        return sb.toString();
     }
 }
