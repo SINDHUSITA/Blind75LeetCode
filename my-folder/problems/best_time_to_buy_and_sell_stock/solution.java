10b5a3dc5705e35347e4ccd080e9de2265f9b8c2
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2022-06-22 18:19:28 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (java)

--- my-folder/problems/best_time_to_buy_and_sell_stock/solution.java
+++ my-folder/problems/best_time_to_buy_and_sell_stock/solution.java
@@ -1,19 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min =Integer.MAX_VALUE;
-        int max =0;
-        int maxProfit = 0;
-        boolean flag = false;
-        for(int i=0;i<prices.length;i++){
-            if(min > prices[i]){
-                min = prices[i];
-                max = prices[i];
-            }else{
-                max = max<prices[i]?prices[i]:max;
-                int diff = max-min;
-                maxProfit = maxProfit<diff ? diff:maxProfit;
+        int size = prices.length - 1;
+            int currMaxValue = prices[size];
+            int maximumProfit = 0;
+            for(int i = size-1; i >= 0; i-- ){
+                 maximumProfit = Math.max(maximumProfit, currMaxValue - prices[i]);
+                 currMaxValue = Math.max( prices[i], currMaxValue);
             }
-        }
-        return maxProfit;
+            return maximumProfit;
+
     }
 }
