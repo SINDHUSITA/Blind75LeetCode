4ff967dc19d0bbd1b418240610bbf3e3263751be
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2023-02-20 23:21:01 +0000

    Sync LeetCode submission - Consecutive Numbers (mysql)

--- my-folder/problems/consecutive_numbers/solution.sql
+++ my-folder/problems/consecutive_numbers/solution.sql
@@ -2,5 +2,5 @@
 
 select distinct l.num as ConsecutiveNums
 from Logs l, Logs l2, Logs l3
-where l.id = l2.id+1 and l2.id =l3.id+1 and l.num= l2.num and l2.num=l3.num
+where l.id+1 = l2.id and l2.id+1 =l3.id and l.num= l2.num and l2.num=l3.num
 
