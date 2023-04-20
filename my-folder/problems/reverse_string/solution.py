94a19fd70006d658d6855404dead757cf40d08b0
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2023-04-20 03:29:22 +0000

    Sync LeetCode submission - Reverse String (python3)

---
+++ my-folder/problems/reverse_string/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        def helper(left, right, s):
+            if left >= right:
+                return
+            s[left], s[right] = s[right], s[left]
+            left, right = left+1, right-1
+            helper(left, right, s)
+        helper(left=0, right=len(s)-1, s=s)
+        
