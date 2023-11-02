0919e6e532b78bd6cb40316f9c97c72ac0d42da5
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2023-11-02 15:18:01 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

---
+++ my-folder/problems/valid_palindrome/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        import re
+        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        print(s)
+        left = 0
+        right = len(s)-1
+        # print(s, s[::-1])
+        # return s == s[::-1]
+        while left < right:
+            if s[left] == s[right]:
+                left+=1
+                right-=1
+            else:
+                return False
+        return True
