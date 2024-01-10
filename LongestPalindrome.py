00709f4463b38416aa339602601814c0eda24255
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2024-01-10 10:01:56 -0600

    Create LongestPalindrome.py

---
+++ LongestPalindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        from collections import Counter
+        char_count = Counter(s)
+        oddCount = [1 for k,v in char_count.items() if v%2 == 1]
+        if oddCount:
+            return len(s)-len(oddCount)+1
+        return len(s)
