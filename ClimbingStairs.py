add6ef99f4449a5e29c196cba945e3fb6c985b11
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2024-01-10 09:50:04 -0600

    Create ClimbingStairs.py

---
+++ ClimbingStairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        from math import comb
+        current = n-1
+        count = 1
+        for i in range(1, n//2 + 1):
+            count += comb(current, i)
+            current -= 1
+        return count
