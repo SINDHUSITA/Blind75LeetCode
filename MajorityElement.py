6f632f3db302ad5ed82d827027e96465bb8ccdaf
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2024-01-11 10:50:42 -0600

    Update MajorityElement.py

--- MajorityElement.py
+++ MajorityElement.py
@@ -1,5 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        s = [i for i in set(nums) if nums.count(i) > len(nums)/2]
-        return s[0]
+        # s = [i for i in set(nums) if nums.count(i) > len(nums)/2]
+        # return s[0]
+        
+        count = 0
+        res = None
+        for num in nums:
+            if count == 0:
+                res = num
+            count += (1 if num == res else -1)
+        return res
         
