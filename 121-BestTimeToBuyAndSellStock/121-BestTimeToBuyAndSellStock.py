e5162dbebc4e783eaffdc9c46c63e361669953f7
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2025-06-12 08:30:28 -0700

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.py
@@ -0,0 +1,27 @@
+# Last updated: 6/12/2025, 8:30:28 AM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        left = 0
+        right = 1 if len(prices) > 1 else None
+        if right == None:
+            return 0
+        while right < len(prices):
+            curProfit = prices[right] - prices[left]
+            if prices[left] < prices[right]:
+                maxProfit = max(curProfit, maxProfit)
+            else:
+                left = right
+            right += 1
+        return maxProfit
+        # for i in range(len(prices)-1):
+        #     left = i
+        #     right = i+1
+        #     if prices[left] < prices[right]:
+        #         # print("left ptr const:", prices[left])
+        #         for j in range(right, len(prices)):
+        #             # print("check profit vals", prices[j], prices[left])
+        #             if prices[j] - prices[left] > maxProfit:
+        #                 maxProfit = prices[j] - prices[left]
+        #                 # print("assign max profit:", maxProfit)
+        return maxProfit
