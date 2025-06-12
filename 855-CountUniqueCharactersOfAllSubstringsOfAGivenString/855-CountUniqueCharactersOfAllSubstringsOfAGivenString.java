4f57cb6dbbdcf9cbcd2e8a558fb500ad81d5742f
Sindhu Sita Inuganti <sindhusita.11@gmail.com>
2025-06-12 08:29:38 -0700

    Create 855-CountUniqueCharactersOfAllSubstringsOfAGivenString

---
+++ 855-CountUniqueCharactersOfAllSubstringsOfAGivenString/855-CountUniqueCharactersOfAllSubstringsOfAGivenString.java
@@ -0,0 +1,27 @@
+// Last updated: 6/12/2025, 8:29:38 AM
+class Solution {
+    public int uniqueLetterString(String s) {
+        int res = 0;
+        // last two indexes
+        int[][] index = new int[26][2];
+        
+        for(int i = 0; i < 26; i++){
+            index[i] = new int[]{-1, -1};
+        }
+        
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            int[] prevIndex = index[s.charAt(i) - 'A'];
+            res += (prevIndex[1] - prevIndex[0]) * (i - prevIndex[1]);   
+            
+            prevIndex[0] = prevIndex[1];
+            prevIndex[1] = i;
+        }
+        
+        for(int[] prevIndex: index){
+            res += (prevIndex[1] - prevIndex[0]) * (n - prevIndex[1]);
+        }
+        
+        return res;
+    }
+}
